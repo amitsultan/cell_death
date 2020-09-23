2cf040d1141caef7cabf7c077fc517235c446026
liatico <liatp@post.bgu.ac.il>
2020-09-23 12:38:57 +0300

    Update data.js

--- cell-death-server/routes/data.js
+++ cell-death-server/routes/data.js
@@ -34,4 +34,23 @@ router.get("/saveData", (req, res) => {
 
 });
 
+router.get("/directories", (req, res) => {
+  let listOfDirectories;
+  const path = require('path');
+  const fs = require('fs');
+
+  const directoryPath = "";
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+      });
+    });
+  });
+
 module.exports = router;
