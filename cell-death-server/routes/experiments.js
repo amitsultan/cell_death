0870d9b5ffc73a7bb0a2e37164479302e1ca82c7
yarinHayun <62398145+yarinHayun@users.noreply.github.com>
2020-10-22 14:52:38 +0300

    commit

--- cell-death-server/routes/experiments.js
+++ cell-death-server/routes/experiments.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var router = express.Router();
-const neatCsv = require('neat-csv');
+const neatCsv = require("neat-csv");
 const { Time } = require("mssql");
+var fs = require("fs");
+// var Unrar = require("unrar");
+var multer = require("multer");
+var upload = multer(); 
+const fileUpload = require('express-fileupload');
+router.use(fileUpload());
+var $ = (jQuery = require("jquery"));
+$.csv = require("jquery-csv");
+
 // router.use(function (req, res, next) {
 //     if (req.session && req.session.user_id) {
 //       DButils.execQuery("SELECT user_id FROM dbo.users")
@@ -32,37 +41,6 @@ router.get("/getImageById/:experimentId/:imageId", (req, res) => {
   }
 });
 
-// router.get("/getImages/:experimentId/:numberOfImages", (req, res) => {
-//   //returns $numberOfImages images from $experimentId and total number of images in the experiment
-//   const fs = require('fs');
-//   const experimentId = req.params.experimentId;
-//   const numberOfImages = req.params.numberOfImages;
-//   //open right directoriy
-//   const path = "C:/programingProjects/data/" + experimentId;
-//   var listOfImages = [];
-//   fs.readdir(path, function (err, files) {
-//     //handling error
-//     if (err) {
-//       res.status(500).send('Unable to get experiment');
-//     }
-//     //sort directory
-//     files.sort();
-//     //get first $numberOfImages images
-//     for(var i=0; i<numberOfImages;i++){
-//       listOfImages.push(path+'/'+files[i]);
-//     }
-//     //get csv file?
-//     res.status(200).send(listOfImages)
-//     return listOfImages
-//   });
-
-//   res.status(200).send(listOfImages)
-
-// });
-
-// router.get("/saveData", (req, res) => {
-
-// });
 
 router.get("/getExperiments", (req, res) => {
   var listOfDirectories = [];
@@ -87,31 +65,29 @@ router.get("/getCsvDataById/:experimentId/:frameId", (req, res) => {
   try {
     const experimentId = req.params.experimentId;
     const frameId = req.params.frameId;
-
-    var fs = require('fs');
-    var $ = jQuery = require('jquery');
-    $.csv = require('jquery-csv');
-    const path = "C:/Users/yarin/Documents/GitHub/cell_death/data/1/1.csv" //change path
+    const path = "../data/" + experimentId + "/" + experimentId + ".csv";
     var listOfData = [];
-    fs.readFile(path, 'UTF-8', function (err, csv) {
+    fs.readFile(path, "UTF-8", function (err, csv) {
       $.csv.toArrays(csv, {}, function (err, data) {
         for (var i = 0, len = data.length; i < len; i++) {
-          if (data[i][3] === frameId)
-            listOfData.push(data[i]);
+          if (data[i][3] === frameId) listOfData.push(data[i]);
         }
       });
-       let listOfDataAfterEditing = EditListOfData(listOfData);
-       console.log(listOfDataAfterEditing);
-       res.status(200).send(listOfDataAfterEditing);   
-    })
-  }catch (err) {
+      if (err) {
+        res.status(500).send("Unable to get csv data");
+      }
+      let listOfDataAfterEditing = EditListOfData(listOfData);
+      console.log(listOfDataAfterEditing);
+      res.status(200).send(listOfDataAfterEditing);
+    });
+  } catch (err) {
     console.log(err);
     res.status(500).send("Unable to get csv data");
-  }    
+  }
 });
 
 function EditListOfData(listOfData) {
-  return listOfData.map((row) => {  
+  return listOfData.map((row) => {
     return {
       id: row[0],
       X: row[1],
@@ -119,7 +95,30 @@ function EditListOfData(listOfData) {
       time: row[3],
     };
   });
-  
 }
 
+
+//upload rar file 
+router.post('/upload', (req, res) => {
+
+  if (!req.files) {
+    return res.status(500).send({ msg: "file is not found" })
+  }
+  // accessing the file
+  const myFile = req.files.file1;
+  console.log(req.files)
+  //  mv() method places the file inside public directory
+  myFile.mv(`${myFile.name}`, function (err) {
+    if (err) {
+      console.log(err)
+      return res.status(500).send({ msg: "Error occured" });
+    }
+    // returing the response with file path and name
+    return res.send({ name: myFile.name, path: `/${myFile.name}` });
+  });
+})
+
+
 module.exports = router;
+
+  
