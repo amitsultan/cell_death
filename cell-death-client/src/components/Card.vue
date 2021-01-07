bff55356a672cdd82a6c340eb026a4f07bb92430
HReyes <reyes@post.bgu.ac.il>
2021-01-07 19:41:02 +0200

    home page and about page - hackathon

---
+++ cell-death-client/src/components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <div class="card-image" v-if="$slots.image">
+      <slot name="image"></slot>
+    </div>
+    <div class="card-header" v-if="$slots.header || title" :class="headerClasses">
+      <slot name="header">
+        <h4 class="card-title">{{title}}</h4>
+        <p class="card-category" v-if="subTitle">{{subTitle}}</p>
+      </slot>
+    </div>
+    <div class="card-body" :class="bodyClasses">
+      <slot>
+          <img loading="lazy" :src="src"/>
+          <!--:src="require(`@/assets/img/${filename}`)"-->
+      </slot>
+    </div>
+    <slot name="raw-content"></slot>
+    <div class="card-footer" :class="footerClasses" v-if="$slots.footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "card",
+    props: {
+      title: {
+        type: String,
+        description: "Card title"
+      },
+      subTitle: {
+        type: String,
+        description: "Card subtitle"
+      },
+      src: {
+        type: String,
+        description: "Card image src"
+      }
+    }
+  };
+</script>
+<style>
+</style>
--- cell-death-client/src/pages/Home-Page.vue
+++ cell-death-client/src/pages/Home-Page.vue
@@ -1,15 +1,58 @@
 <template>
-<div>
-  <canvas id='tif_image'></canvas>
+ <div class="container shadow-lg p-3 mb-5 bg-white rounded left">
+    <h1>
+        Welcome to Cell Tracking Tool 
+    </h1>
+    <div class="home_text">
+    Our cell tracking tool is a multi-purpose tool for various microscope images analysis.
+    Our goal is to provide an easy way to detect certain states of cells or other moving organisms.
+    Our tool will serve for phenomena as Cell Death and Calcium Signaling, hopefully for more in the future.
+     <br><br>Enjoy your visit in our site :) 
+    </div>
+    <br><br>
+    <div class="bottom">
+    <img class='gif' loading="lazy" src="../assets/celldeath.gif">
+
+    <img class='gif' loading="lazy"  src="../assets/calcium.gif">
+    </div>
+    <!--<video-player :options="videoOptions"/>-->
 </div>
 </template>
 
 <script>
+
 export default {
  name: "App",
-}
+ }
 </script>
-
 <style>
-
+.left {
+  font-family: "Comic Sans MS", cursive, sans-serif;
+  text-align: left;
+  margin-left: 100px;
+  margin-top: 30px;
+}
+.right {
+  text-align: right;
+  margin-right: 200px;
+}
+.bottom {
+  margin-top: 200px;
+  margin-left: 100px;
+}
+.gif {
+  
+  width:320px;
+  height:280px;
+  margin: 15px 15px 15px 15px;
+}
+.gif:hover { 
+transform: scale(1.1); 
+}
+.home_text{
+  font-size: 6mm ;
+  position: fixed;
+  margin: 15px 0px 0px 20px;
+  width: 1000px
+}
 </style>
