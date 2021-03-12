614057f43c26947f471cae3f11811f713e5fbbb5
yarinHayun <62398145+yarinHayun@users.noreply.github.com>
2021-03-12 12:10:24 +0200

    update nav bar

--- cell-death-client/src/App.vue
+++ cell-death-client/src/App.vue
@@ -12,27 +12,24 @@
           <b-nav-item :to="{ name: 'AboutPage'}">About</b-nav-item>
           <b-nav-item :to="{ name: 'ContactPage'}">Contact</b-nav-item>
         </b-navbar-nav>
-        <!-- Modal for requesting experiemnts   -->
+  
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
             <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
             <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
           </b-nav-form>
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
-              <em>User</em>
-            </template>
-            <div v-if="$root.store.email">
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item v-on:click='Logout'>Sign Out</b-dropdown-item>
-            </div>
-            <div v-else>
+          <b-nav-item-dropdown right v-if="!$root.store.email" text="Hello guest">
+            <div>  
               <b-dropdown-item :to="{ name: 'login' }">Login</b-dropdown-item>
               <b-dropdown-item :to="{ name: 'register' }">Sign up</b-dropdown-item>
             </div>
           </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right v-if="$root.store.email" text="user">
+              <b-dropdown-item v-on:click='Logout'>Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
