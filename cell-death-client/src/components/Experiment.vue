84729b5135ab47909f535beca9e324f0a8b61b70
HReyes <reyes@post.bgu.ac.il>
2021-03-13 19:19:50 +0200

    fixed display coodinations

--- cell-death-client/src/components/Experiment.vue
+++ cell-death-client/src/components/Experiment.vue
@@ -53,7 +53,7 @@
         <td>{{ row.id }}</td>  
         <td>{{ row.x }}</td>              
         <td>{{ row.y }}</td>
-        <td>{{ row.frame }}</td>            
+        <td>{{ row.frame }}</td>                   
     </template>
         <template v-slot:no-data>
       <span>No data</span>
@@ -158,7 +158,7 @@ export default {
             let left = e.clientX;
             let top = e.clientY;
             this.pause_mark.x = e.offsetX;
-            this.pause_mark.y = e.offsetY;
+            this.pause_mark.y = this.height - e.offsetY;
             let menuBox = window.document.querySelector(".menu");
             menuBox.style.left = left + "px";
             menuBox.style.top = top + "px";
@@ -259,7 +259,7 @@ export default {
             this.mark.y = y;
             const mark = {
                 x: x,
-                y: y,
+                y: this.height - y,
                 frame:this.current-1,
                 type: this.type,
                 color: this.typeColor(this.type)
@@ -301,7 +301,7 @@ export default {
                 ctx.beginPath();
                 ctx.strokeStyle = mark.color // line color
                 ctx.fillStyle = mark.color; // line color
-                ctx.arc(mark.x, mark.y, 6, 0, 2 * Math.PI);
+                ctx.arc(mark.x, this.height - mark.y, 6, 0, 2 * Math.PI);
                 // ctx.rect(mark.x + 12, mark.y - 29, 1, 22);
                 // ctx.font = "20px Arial";
                 // ctx.fillText(mark.type, mark.x + 16, mark.y - 15);
@@ -431,7 +431,7 @@ export default {
     async created() {
         this.image = new Image();
         let config = {
-            url: this.$root.API_BASE + "experiments/getDetails/"+this.id,
+            url: this.$root.API_BASE + "experiments/getDetails/"+this.id,//"20180514"
             method: 'GET'
         }
         await this.axios(config).then((response) =>{
@@ -444,7 +444,7 @@ export default {
                     "danger"
                 );
             }
-        })
+        }).catch((err)=>{console.log(err)})
         // this.image.src = '@/assets/images/c2_ (1).png'; //   '../assets/images/c2_ (1).png';
     },
     async beforeMount(){
