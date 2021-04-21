355190fda6dcea82bc76c2ed779adbe4fad026ce
liatico <liatp@post.bgu.ac.il>
2021-04-21 17:36:58 +0300

    added registration email

--- cell-death-server/routes/auth.js
+++ cell-death-server/routes/auth.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const bcrypt = require("bcrypt");
 var DButils = require('../DB/DButils')
-
+const mailController = require('../controllers/mailerController')
 
 // middleware function to check for logged-in users
 // throws user back to home page if logged-in
@@ -18,45 +18,47 @@ var sessionChecker = (req, res, next) => {
 
 router.post("/Register", sessionChecker, async (req, res, next) => {
   // missing parameters
-  try {
-    if (
-      !req.body.firstname ||
-      !req.body.lastname ||
-      !req.body.email ||
-      !req.body.password
-    ) {
-      throw { status: 500, message: "one or more of the details is missing" };
-    }
-    DButils.execQuery("SELECT email FROM users")
-      .then((users) => {
-        if (users.find((x) => x.email === req.body.email))
-          throw { status: 422, message: "Email already exists!" };
-        let hash_password = bcrypt.hashSync(
-          req.body.password,
-          // parseInt(process.env.bcrypt_saltRounds)
-          parseInt(13)
-        );
-        const userDetails = {
-          fname: req.body.firstname,
-          lname: req.body.lastname,
-          email: req.body.email,
-          password: hash_password,
-        };
-        DButils.execRegister(userDetails)
-          .then((results) => {
-            res.status(200).send({ message: "User created!", success: true });
-          })
-          .catch((err) => {
-            throw { status: 500, message: "Error occurred" };
-          });
-      })
-      .catch((error) => {
-        next(error);
-      });
-  } catch (error) {
-    next(error);
+  try{
+    if (!req.body.firstname ||
+    !req.body.lastname ||
+    !req.body.email ||
+    !req.body.password)
+      return res.send({ status: 500, message: "one or more of the details is missing" });
   }
-});
+  catch(error)
+  {
+    return res.send({ status: 500, message: "one or more of the details is missing" });
+  }
+  let users = 0 
+  try{
+    users = await DButils.userByEmail(req.body.email)
+    if (users.length>0)
+      return res.send({ status: 422, message: "Email already exists!" });
+  }catch(error)
+  {
+    next(error)
+  }
+  let hash_password = bcrypt.hashSync(
+    req.body.password,
+      // parseInt(process.env.bcrypt_saltRounds)
+    parseInt(13)
+  );
+  const userDetails = {
+    fname: req.body.firstname,
+    lname: req.body.lastname,
+    email: req.body.email,
+    password: hash_password,
+  };
+  try{
+    await DButils.execRegister(userDetails)
+    res.status(200).send({ message: "User created!", success: true });
+    mailController.sendRegistrationMail(req.body.email, req.body.firstname, req.body.lastname)
+  }catch(error){
+    next(error)
+  }
+})
+  
+
 
 router.post("/Login", sessionChecker, async (req, res, next) => {
   try {
